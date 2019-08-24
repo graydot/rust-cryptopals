d9763d58433c2db44c8676f8a8b8336c8b8b5d16
Jeba Singh Emmanuel <jebasingh.emmanuel@gmail.com>
2019-08-23 18:54:26 -0700

    - Changed from main.rs to lib.rs - this is not a binary package, it is a library so i can reuse code - Added live coding with Brandon - c3 - base64 utility functions work, tests are broken
    Next step - write tests for base64 and start pulling more functions to base64 and utils files

---
+++ src/base64.rs
@@ -0,0 +1,37 @@
+use std::num::ParseIntError;
+const BASE64_MAP: &[u8; 64] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+pub fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, ParseIntError> {
+    hex.as_bytes().chunks(2).map(|chunk| {
+        u8::from_str_radix(&format!("{}{}", char::from(chunk[0]), char::from(chunk[1])), 16)
+    }).collect()
+}
+
+pub fn bytes_to_base64(bytes: &[u8]) -> String {
+    bytes.chunks(3).fold(String::new(), |mut acc, chunk| {
+        let first = (chunk[0] & 0b1111_1100) >> 2;
+        let second = (chunk[0] & 0b0000_0011) << 4 | chunk[1] >> 4;
+        let third = (chunk[1] & 0b0000_1111) << 2 | chunk[2] >> 6;
+        let fourth = chunk[2] & 0b0011_1111;
+        acc.push_str(&format!("{}", char::from(BASE64_MAP[first as usize]))); 
+        acc.push_str(&format!("{}", char::from(BASE64_MAP[second as usize]))); 
+        acc.push_str(&format!("{}", char::from(BASE64_MAP[third as usize]))); 
+        acc.push_str(&format!("{}", char::from(BASE64_MAP[fourth as usize]))); 
+
+        acc
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hex_2_base64(){
+        assert_eq!(hex_2_base64(&"".to_string()), "".to_string());
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string();
+        let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string();
+        assert_eq!(hex_2_base64(&input), output);
+
+    }
+}
--- src/bin/set1_c1.rs
+++ src/bin/set1_c1.rs
@@ -1,73 +1,11 @@
-use itertools::Itertools;
+use cryptopals::base64::*;
 
-const BASE64_MAP: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 fn main() {
     let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string();
     let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string();
-    assert_eq!(hex_2_base64(&input), output);
-}
-/// # Examples
-/// ```
-/// let hex = String::new("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
-/// let base64 = String::new("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
-/// assert_eq!(hex_2_base64(&hex), base64);
-/// ```
-fn hex_2_base64(hex: &str) -> String {
-    // println!("Chars: {:?}", hex.chars());
-    let bytes = hex.chars().map(|ch| 
-        match ch.to_digit(16) {
-            Some(num) => num,
-            None => 0,
-    } as u16
-    );
-    // combine next loop
-    let mut base64_values: Vec<u16> = Vec::new(); 
-    // chunks.map so i get tuples
-    for mut combination in &bytes.chunks(3){
-        let elem0 = match combination.next() {
-            Some(num) => num,
-            None => 0,
-        };
-        let elem1 = match combination.next() {
-            Some(num) => num,
-            None => 0,
-        }; 
-        let elem2 = match combination.next() {
-            Some(num) => num,
-            None => 0,
-        };
-        // use bitwisehere
-        let two_six_bits = (elem0 * u16::pow(2, 8) + elem1 * u16::pow(2, 4) + elem2)as u16;
-        let two_pow_6 = u8::pow(2,6) as u16;
-        base64_values.insert(0, two_six_bits/two_pow_6);
-        base64_values.insert(0, two_six_bits % two_pow_6);
-    };
-
-    let base64_strings: Vec<&str>= base64_values.iter().map(|value|
-    {
-        let start = *value as usize;
-        let end = start + 1;
-        match BASE64_MAP.get(start..end) {
-            Some(chr) => chr,
-            None => "",
-        }
-    }).rev().collect();
-    let return_value = base64_strings.iter().fold(String::new(), |acc, &str_val| {
-        acc + str_val
-    });
-    return_value
+    let bytes = hex_to_bytes(&input.to_string()).unwrap();
+    let base64 = bytes_to_base64(&bytes);
+    assert_eq!(base64, output);
+    // assert_eq!(hex_2_base64(&input), output);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_hex_2_base64(){
-        assert_eq!(hex_2_base64(&"".to_string()), "".to_string());
-        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string();
-        let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string();
-        assert_eq!(hex_2_base64(&input), output);
-
-    }
-}
