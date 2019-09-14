9a19f2b61d35111cdc73f38a26a3f85f27084022
Jeba Singh Emmanuel <jebasingh.emmanuel@gmail.com>
2019-09-13 22:49:37 -0700

    Actually printed out the solution for problem 7

--- src/bin/set1_c7.rs
+++ src/bin/set1_c7.rs
@@ -6,17 +6,12 @@ const CODE:&[u8] = b"YELLOW SUBMARINE";
 
 pub fn main() {
     let file_contents = get_file_contents("data/set1_c7.txt").unwrap();
-    println!("{}", file_contents.len());
     let encrypted_vector = base64_to_bytes(&file_contents);
     let encrypted_bytes = encrypted_vector.as_slice();
-    println!("{:?}", &encrypted_bytes);
-    println!("{}", &encrypted_bytes.len());
     let cipher = Cipher::aes_128_ecb();
     let key = CODE;
     let iv: Option<&[u8]> = None;
 
     let output = decrypt(cipher, key, None, &encrypted_bytes);
-    println!("{:?}", output.unwrap());
-
-
+    println!("{}", bytes_to_string(output.unwrap().as_slice()));
 }
